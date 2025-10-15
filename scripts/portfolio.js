23e72f9ee5250b6fbf28fae0bee0c756f980ccda
Vy Nguyen <103297949+vyrnguyen@users.noreply.github.com>
2025-10-15 13:31:17 -0700

    Created new portfolio directory and updated paths

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,43 +1,14 @@
 const projectSection = document.querySelector(".projects");
 const card = document.querySelector(".card");
 const projectInitDisplay = projectSection.style.display;
-const btn = document.querySelector(".btn");
-
-function fadeOut(element) {
-  var i = 1;
-  var timer = setInterval(function () {
-    if (i <= 0) {
-      clearInterval(timer);
-      element.style.display = "none";
-    }
-    element.style.opacity = i;
-    i -= 0.1;
-  }, 50);
-}
-
-function fadeIn(element) {
-  var i = 0;
-  element.style.display = projectInitDisplay; 
-  var timer = setInterval(function () {
-    if (i >= 1) {
-      clearInterval(timer);
-    }
-    element.style.opacity = i;
-    i += 0.1;
-  }, 50);
-}
 
 var isFaded = false;
 card.addEventListener("click", () => {
   if (isFaded) {
-    fadeIn(projectSection);
+    projectSection.style.animation = "fade-in 0.5s forwards";
     isFaded = false;
   } else {
-    fadeOut(projectSection);
+    projectSection.style.animation = "fade-out 0.5s forwards";
     isFaded = true;
   }
 });
-
-btn.addEventListener("click", () => {
-  fadeIn(projectSection);
-})
