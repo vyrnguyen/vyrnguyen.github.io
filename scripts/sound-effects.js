bc1ee8111bed10debcce204c91b8db78f4b2f3c0
Vy Nguyen <103297949+vyrnguyen@users.noreply.github.com>
2025-11-02 16:28:00 -0800

    Remove sound assets

--- scripts/sound-effects.js
+++
@@ -1,9 +0,0 @@
-var click = document.getElementById("click-sound");
-const sfx = document.getElementsByClassName("sfx");
-
-for (let i = 0; i < sfx.length; i++) {
-  sfx[i].addEventListener("click", function() {
-    click.currentTime = 0;
-    click.play();
-  })
-}
