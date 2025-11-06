35c96ab16e88e0d52d9d7ec783ef47d401c0a231
Vy Nguyen <103297949+vyrnguyen@users.noreply.github.com>
2025-11-05 21:53:52 -0800

    fix(ui): try using pJS's dom to change color

--- scripts/theme.js
+++ scripts/theme.js
@@ -33,22 +33,44 @@ document.addEventListener("DOMContentLoaded", () => {
   if (mode == null) {
     // dont need to set anything; base by default
     localStorage.setItem("theme", "light");
+
+    if (window.pJSDom && window.pJSDom.length > 0) {
+      window.pJSDom[0].pJS.particles.color.value = "#54433a" 
+      window.pJSDom[0].pJS.particles.line_linked.color = "#54433a" 
+      window.pJSDom[0].pJS.fn.particlesRefresh();
+    }
   } else if (mode == "dark") {
     root.classList.toggle("dark");
-    particlesjs.classList.toggle("dark");
+    // particlesjs.classList.toggle("dark");
     setDarkTheme();
+
+    if (window.pJSDom && window.pJSDom.length > 0) {
+      window.pJSDom[0].pJS.particles.color.value = "#fffcfa" 
+      window.pJSDom[0].pJS.particles.line_linked.color = "#fffcfa" 
+      window.pJSDom[0].pJS.fn.particlesRefresh();
+    }
   }
 });
 
 function toggleTheme() {
   root.classList.toggle("dark");
-  particlesjs.classList.toggle("dark");
+  // particlesjs.classList.toggle("dark");
 
   var mode = localStorage.getItem("theme");
   if (mode == "light") {
     setDarkTheme();
+    if (window.pJSDom && window.pJSDom.length > 0) {
+      window.pJSDom[0].pJS.particles.color.value = "#fffcfa" 
+      window.pJSDom[0].pJS.particles.line_linked.color = "#fffcfa" 
+      window.pJSDom[0].pJS.fn.particlesRefresh();
+    }
   } else {
     setLightTheme();
+    if (window.pJSDom && window.pJSDom.length > 0) {
+      window.pJSDom[0].pJS.particles.color.value = "#54433a" 
+      window.pJSDom[0].pJS.particles.line_linked.color = "#54433a" 
+      window.pJSDom[0].pJS.fn.particlesRefresh();
+    }
   }
 }
 
