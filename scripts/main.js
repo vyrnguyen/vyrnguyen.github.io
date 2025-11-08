1fe0ca3714ce2703db1272f31bc51d2fdc2b80e0
Vy Nguyen <103297949+vyrnguyen@users.noreply.github.com>
2025-11-07 16:25:45 -0800

    fix(menu): save main js file so that index renders properly

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ var messagesToType = [
   "os-enthusiast",
   "video game maker",
 ];
-var menuIcon = document.getElementById("menu-icon");
 
 function typewriter(i = 0, text = "", callback) {
   if (i < text.length) {
@@ -37,31 +36,4 @@ function startTypingAnimation(i) {
 
 document.addEventListener("DOMContentLoaded", function () {
   startTypingAnimation(0);
-});
-
-// Menu Icons for mobile view
-menuIcon.addEventListener("click", function () {
-  var navLinks = document.getElementById("nav-links");
-  var navLinksStyle = window.getComputedStyle(navLinks);
-
-  if (navLinksStyle.getPropertyValue("display") === "none") {
-    navLinks.style.display = "flex";
-    menuIcon.classList.remove("bi-three-dots");
-    menuIcon.classList.add("bi-x");
-  } else {
-    navLinks.style.display = "none";
-    menuIcon.classList.add("bi-three-dots");
-    menuIcon.classList.remove("bi-x");
-  }
-});
-
-window.addEventListener("resize", function() {
-  var navLinks = document.getElementById("nav-links");
-  if (window.innerWidth > 600) {
-    navLinks.style.display = "flex";
-  } else {
-    navLinks.style.display = "none";
-    menuIcon.classList.add("bi-three-dots");
-    menuIcon.classList.remove("bi-x");
-  }
 });
